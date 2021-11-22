0086e89f32df06eba5f002149cc41793a3723070
dpdtydz <dpdtydz@donga.ac.kr>
2021-11-22 19:54:54 +0900

    spring start!
    convert

---
+++ src/main/java/com/cdp/tdp/TdpApplication.java
@@ -0,0 +1,13 @@
+package com.cdp.tdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdp/tdp/TdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdp.tdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
