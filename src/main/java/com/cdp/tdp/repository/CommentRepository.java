a999a9df8c4e23413556f29d736f20b16473d8b2
leehosang <38107948+dpdtydz@users.noreply.github.com>
2021-12-07 16:11:07 +0900

    Revert "Lhs"

--- src/main/java/com/cdp/tdp/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cdp.tdp.repository;
-
-import com.cdp.tdp.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByTilId(Long til_id);
-}
