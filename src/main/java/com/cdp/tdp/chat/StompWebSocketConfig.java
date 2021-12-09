b1aded6a8afd926f5761b7e80a45fdf6a2d31a9f
dpdtydz <dpdtydz@donga.ac.kr>
2021-12-10 07:50:35 +0900

    Update StompWebSocketConfig.java

--- src/main/java/com/cdp/tdp/chat/StompWebSocketConfig.java
+++ src/main/java/com/cdp/tdp/chat/StompWebSocketConfig.java
@@ -17,7 +17,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("http://www.tildp.shop")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
