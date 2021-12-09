8a22a7f700ff0a9bb35ad90bedddca390e57d55d
dpdtydz <dpdtydz@donga.ac.kr>
2021-12-09 23:15:38 +0900

    Update Legacy+ Url issue +back.js

--- src/main/resources/static/back.js
+++ src/main/resources/static/back.js
@@ -1,6 +1,6 @@
 let urlEndpoint = '/subscribe';
 let eventSource = new EventSource(urlEndpoint);
-let domainURL= 'https://api.tildp.shop';
+let domainURL= '/api.tildp.shop';
 eventSource.addEventListener("latestNews", function (event) {
     let articleData = JSON.parse(event.data);
     let title = articleData.tilTitle;
@@ -16,19 +16,12 @@ function toast_close(){
 }
 
 
-function token_check(){
-    if (localStorage.getItem('token') == null) {
-        alert("로그인 해주세요")
-        location.href = `${domainURL}/index.html`
-    }
-}
-
 function login_check(options, originalOptions, jqXHR){
     if(localStorage.getItem('token')) {
         jqXHR.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('token'));
     } else {
         alert("로그인 해주세요")
-        location.href =  `${domainURL}/index.html`
+        location.href =  `/index.html`
     }
 }
 
@@ -38,4 +31,8 @@ function sign_out() {
     localStorage.removeItem('username');
     alert('로그아웃!')
     window.location.href = "/"
+}
+
+function goback() {
+    window.history.back();
 }
