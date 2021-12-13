f37c9b550c09f6a2c4da6ccc8573ac1ce691d8bc
dduoming <dltndudvldzm@naver.com>
2021-12-13 15:10:17 +0900

    For 웹소켓 오류해결#2  url 실수

--- src/main/resources/static/back.js
+++ src/main/resources/static/back.js
@@ -1,7 +1,7 @@
 let urlEndpoint = 'http://api.tildp.shop/subscribe';
 let eventSource = new EventSource(urlEndpoint);
 let domainURL= 'http://api.tildp.shop';
-let domainURL= 'http://localhost:8080';
+// let domainURL= 'http://localhost:8080';
 eventSource.addEventListener("latestNews", function (event) {
     let articleData = JSON.parse(event.data);
     let title = articleData.tilTitle;
