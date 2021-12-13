86a22c0bd570e7cbc228bba4a43e2512b17a0680
Hyeyoung Kim <sunzero0116@gmail.com>
2021-12-13 20:37:03 +0900

    자신의 게시글과 public만 til_board에 띄우기
    pagination과 private / public 연결완료

--- src/main/java/com/cdp/tdp/repository/TilRepository.java
+++ src/main/java/com/cdp/tdp/repository/TilRepository.java
@@ -14,9 +14,9 @@ import java.util.Optional;
 
 public interface TilRepository extends JpaRepository<Til, Long> {
     // get all til
-    List<Til> findByTilViewOrderByIdDesc(boolean tilView);
+    List<Til> findByTilViewOrUserIdOrderByIdDesc(boolean tilView, Long userId);
     // get all til using pagination
-    Page<Til> findByTilViewOrderByIdDesc(boolean tilView, Pageable pageable);
+    Page<Til> findByTilViewOrUserIdOrderByIdDesc(boolean tilView, Long userId, Pageable pageable);
     // get one til by id
     Optional<Til> findById(Long til_id);
 
@@ -31,13 +31,11 @@ public interface TilRepository extends JpaRepository<Til, Long> {
     int minusLike(Long id);
 
     // get my til
-    @Query("select til from Til til order by til.id DESC")
-    List<Til> findByUser(User user);
+    List<Til> findByUserOrderByIdDesc(User user);
 
     // get search output
-    Page<Til> findByUserOrderByIdDesc(User user, Pageable pageable);
-    Page<Til> findByTilTitleOrderByIdDesc(String til_title, Pageable pageable);
-    Page<Til> findByTagsNameOrderByIdDesc(String keyword, Pageable pageable);
-//    findAllByTil_viewOrUserId(boolean true, Long id)
+    Page<Til> findByUserOrUserIdOrderByIdDesc(User user, boolean tilView, Long userId, Pageable pageable);
+    Page<Til> findByTilTitleOrUserIdOrderByIdDesc(String tilTitle, boolean tilView, Long userId, Pageable pageable);
+    Page<Til> findByTagsNameOrUserIdOrderByIdDesc(String keyword, boolean tilView, Long userId, Pageable pageable);
 }
 
