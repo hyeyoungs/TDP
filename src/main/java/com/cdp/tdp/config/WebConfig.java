56174a85d9eb1b886f6912924cb492a7ddbab826
Hyeyoung Kim <sunzero0116@gmail.com>
2021-12-14 03:07:42 +0900

    WebConfig 원상복귀

--- src/main/java/com/cdp/tdp/config/WebConfig.java
+++ src/main/java/com/cdp/tdp/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("POST", "PUT", "GET", "HEAD", "OPTIONS", "DELETE");
     }
 }
