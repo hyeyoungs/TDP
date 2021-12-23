cba2e2690dfc189229d96bd81993ad7917afad76
Hyeyoung Kim <sunzero0116@gmail.com>
2021-12-23 17:10:16 +0900

    Update EbHashCheckController.java

--- src/main/java/com/cdp/tdp/controller/EbHashCheckController.java
+++ src/main/java/com/cdp/tdp/controller/EbHashCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EbhashcheckController {
+public class EbHashCheckController {
     @GetMapping( "/")
     public ResponseEntity<?> doHealthCheck() {
         return ResponseEntity.ok("HealthCheck OK");
