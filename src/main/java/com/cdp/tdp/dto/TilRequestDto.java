968b7a6f3077d62dfcf830b27920ddb2bed1f5fe
dduoming <dltndudvldzm@naver.com>
2022-01-06 19:30:35 +0900

    test

--- src/main/java/com/cdp/tdp/dto/TilRequestDto.java
+++ src/main/java/com/cdp/tdp/dto/TilRequestDto.java
@@ -1,10 +1,16 @@
 package com.cdp.tdp.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.transaction.Transactional;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TilRequestDto {
     //제목,본문,작성자,보기권한
     private String tilTitle;
